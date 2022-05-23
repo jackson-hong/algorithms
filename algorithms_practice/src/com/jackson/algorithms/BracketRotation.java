4b24df3b033c8e31f9698806927820e257e77db4
Jackson Hong <jacksonhong0111@gmail.com>
2022-05-23 22:05:49 +0900

    프로그래머스 괄호 회전하기

---
+++ algorithms_practice/src/com/jackson/algorithms/BracketRotation.java
@@ -0,0 +1,44 @@
+package com.jackson.algorithms;
+
+public class BracketRotation {
+
+    private static String[] rightStrArr = new String[] {"()","[]","{}"};
+
+    public static void main(String[] args) {
+        String ex1 = "[](){}";
+        String ex2 = "}]()[{";
+        String ex3 = "[)(]";
+        String ex4 = "}}}";
+
+        System.out.println(solution(ex1) == 3);
+        System.out.println(solution(ex2) == 2);
+        System.out.println(solution(ex3) == 0);
+        System.out.println(solution(ex4) == 0);
+    }
+
+    private static int solution(String s) {
+        int answer = 0;
+
+        if(s.length() == 0) return 0;
+
+        for(int i = 0; i < s.length(); i++){
+            char firstChar = s.charAt(0);
+            s = s.substring(1) + firstChar;
+            if(validStr(s))answer++;
+        }
+
+        return answer;
+    }
+
+    private static boolean validStr(String s) {
+        String temp = s;
+        for(int i = 0; i < rightStrArr.length; i++){
+            if(temp.contains(rightStrArr[i])){
+                temp = temp.replace(rightStrArr[i], "");
+                i = 0;
+            }
+            if(temp.equals(""))return true;
+        }
+        return false;
+    }
+}
